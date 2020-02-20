25528fd203b084b80b4f3328903711e090bea937
harish1200 <60343266+harish1200@users.noreply.github.com>
2020-02-20 19:34:29 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,60 +1,25 @@
-package calculator;
+import java.util .*;
 
-import java.util.*;
+ class SiCi
+{
+ public static void main(String[] args) {
+ double p, rate, t, si, ci;
+        System.out.print("Enter The Amount : ");
+        Scanner read = new Scanner(System. in);
+     p = read.nextDouble();
 
-	//Simple calculator
+     System. out. print("Enter The No. of Years : ");
+    t = read.nextDouble();
 
-	public class calc
-	{
-	    public static void main(String[] args)
-	    {
-	        @SuppressWarnings("resource")
-			Scanner in = new Scanner(System.in);
-	        double a, b, c = 0.0;
+     System. out. print("Enter The Rate of Interest (ROI) : ");
+     rate = read.nextDouble();
 
-	        System.out.print("Enter the first number \n");
-	        a = in.nextDouble();
-	        System.out.print("And enter second  \n");
-	        b = in.nextDouble();
-	        System.out.print("Choose the operation  " +
-	              "\n1.Addition" +
-	        		"\n2.Subtraction" +
-	        		"\n3.Multiplication" +
-	        		"\n4.Division" +
-	        		"\n5.Power" +
-	        		"\n#Please enter the number of operation \n");
-	        double somethin = in.nextDouble();
-	        double addition = 1;
-	        double subtraction = 2;
-	        double multiplication = 3;
-	        double division = 4 ;
-	        double power = 5;
-	        if (somethin == addition) {
-	        	c = a + b;  
-	        	System.out.println(a + " + " + b + " = " + c);  }
-	        else if (somethin == subtraction)  {
-	        	c = a - b;  
-	        	System.out.println(a + " - " + b + " = " + c);	}
-	        else if (somethin == multiplication)	{
-	        	c = a * b;   
-	        	System.out.println(a + " * " + b + " = " + c);	}
-	        else if (somethin == division)   {
-	        	c = a / b;  
-	        	System.out.println(a + " / " + b + " = " + c);	}
-	        else if (somethin == power)	{
-	        	System.out.println("Enhance A or B?" +
-	        			"\n1.A" +
-	        			"\n2.B");
-	        	double enhance = in.nextDouble();
-	        	double first = 1;
-	        	double second = 2;
-	        		if (enhance == first) {
-	        			System.out.println(Math.pow(a, 2)); }	
-	        		else if (enhance == second); {
-	        			System.out.println(Math.pow(b, 2)); }
-	        
-	        }
-	    }
-	}	
+     /Calculating Simple Interest and Compound Interest/
+     si = (p * t * rate)/100;
+     ci = p*Math.pow(1.0+rate/100.0,t)-p;
 
-
+     /Displaying Simple Interest and Compound Interest/
+     System.out.println("Simple Interest = "+si);
+     System.out. println("Compound Interest = "+ci);
+ }
+}
